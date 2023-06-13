5e8d8d5f610fbe623349a05200cd7f55af0eab7f
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2023-06-14 00:05:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@
 int main()
 {
     TodoList todoList1;
-    todoList1.addTask("TODO", "aaaaaaaaaaaaaaa", "2023-06-14 06:00:00");
-    todoList1.addTask("HELLO", "bbbbbbbbbbbb", "2024-01-30 12:00:00");
+    todoList1.addTask("Task 1", "Here is a description of Task 1.", "2023-06-14 06:00:00");
+    todoList1.addTask("Task 2", "Here is a description of Task 2.", "2024-01-30 12:00:00");
     todoList1.displayTasks();
-    //  todoList1.deleteTask("TODO");
-    todoList1.isCompleted("TODO");
-    todoList1.modifyTask("TODO", "changeddddddd", "2026-07-11 23:00:00");
-    todoList1.changeDeadline("HELLO", "2023-06-14 01:00:00");
+    //  todoList1.deleteTask("Task 1");
+    todoList1.isCompleted("Task 1");
+    todoList1.modifyTask("Task 1", "Changed description", "2026-07-11 23:00:00");
+    todoList1.changeDeadline("Task 2", "2023-06-14 01:00:00");
     todoList1.displayTasks();
-    std::cout << todoList1.getTaskCount() << std::endl;
-    std::cout << todoList1.getCompletedTaskCount() << std::endl;
-    std::cout << todoList1.getUncompletedTaskCount() << std::endl;
+    std::cout << "Count of tasks : " << todoList1.getTaskCount() << std::endl;
+    std::cout << "Count of completed tasks : " << todoList1.getCompletedTaskCount() << std::endl;
+    std::cout << "Count of uncompleted tasks : " << todoList1.getUncompletedTaskCount() << std::endl;
     todoList1.checkDeadline();
 }
